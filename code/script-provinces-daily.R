d52b35a881e8eefdff1fb8c9b4db5b8366b885bd
antonio_fernandez <antonio.fernandez@imdea.org>
2020-09-19 12:59:17 +0200

    latest

--- code/script-provinces-daily.R
+++ code/script-provinces-daily.R
@@ -323,4 +323,6 @@ for (j in 1:length(dates)){
   write.csv(dw[dw$date == dates[j], ], paste0(estimates_path, country_iso, "/", country_iso, "-", dates_dash[j], "-estimate.csv"), row.names = FALSE)
 }
 
+write.csv(dw[dw$date == dates[length(dates)], ], paste0(estimates_path, country_iso, "/", country_iso, "-latest-estimate.csv"), row.names = FALSE)
+
 }
