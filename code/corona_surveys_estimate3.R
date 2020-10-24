39249d346203b37386c2aa83b91fe01ed49a9234
antonio_fernandez <antonio.fernandez@imdea.org>
2020-10-24 17:45:47 +0200

    ccfr2

--- code/corona_surveys_estimate3.R
+++ code/corona_surveys_estimate3.R
@@ -13,7 +13,7 @@ library(httr)
 
 # compute nee decentralized estimates
 try(source("script-confirmed.R"), silent = T)
-try(source("script-ccfr-based.R"), silent = T)
+try(source("script-ccfr-based2.R"), silent = T)
 #source("script-ccfr-based.R")
 try(source("script-30responses.R"), silent = T)
 try(source("script-300responses.R"), silent = T)
