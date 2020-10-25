adfd584bdeede3d4950411b81d757f4efa0c380a
Oluwasegun Ojo <oluwasegun.ojo@imdea.org>
2020-10-25 19:29:05 +0100

    update ccfr ests with greedy smoothing

--- code/smooth_greedy.R
+++ code/smooth_greedy.R
@@ -3,14 +3,19 @@
 # before the series terminates
 
 smooth_greedy <- function(dt_column){
-  l <- length(dt_column)
   dt_column[is.na(dt_column)] <- 0
-  for (i in 2:l){
-    if (dt_column[i] < dt_column[i-1])  { # track ecc instead
-      d <- dt_column[i-1] - dt_column[i] 
-      dt_column[i] <- dt_column[i-1]
+  ecc <- dt_column[1]
+  for (i in 2:length(dt_column)){
+    if (dt_column[i] < ecc[i-1])  { # track ecc instead
+      d <- ecc[i-1] - dt_column[i] 
+      ecc[i] <- ecc[i-1]
       dt_column[i+1] <- dt_column[i+1] - d
     }
+    else{
+      ecc[i] <- dt_column[i]
+    }
   }
-  return(dt_column[1:l])
+  return(ecc)
 }
+
+
