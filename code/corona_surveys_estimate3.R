e29c6bce86997ec14f540aedeeed2cd0fbf2548d
antonio_fernandez <antonio.fernandez@imdea.org>
2020-11-04 16:08:52 +0100

    madrid

--- code/corona_surveys_estimate3.R
+++ code/corona_surveys_estimate3.R
@@ -17,6 +17,7 @@ try(source("script-ccfr-based3.R"), silent = T)
 try(source("script-ccfr-fatalities.R"), silent = T)
 
 try(source("script-umd_batch_symptom_country.R"), silent = T)
+try(source("script-umd_batch_symptom_region.R"), silent = T)
 
 try(source("script-30responses.R"), silent = T)
 try(source("script-300responses-v2.R"), silent = T)
