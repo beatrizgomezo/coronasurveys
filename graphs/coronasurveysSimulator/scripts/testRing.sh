840860e713b7cdbbba55f0a7557490f32a9036d8
Davide Frey <frey@quark-3.local>
2020-12-08 15:11:27 +0100

    adding script

---
+++ graphs/coronasurveysSimulator/scripts/testRing.sh
@@ -0,0 +1,91 @@
+# docker build -t coronasurveys:debugging .
+
+F=0.01
+S=300
+r=100
+
+E=0.01
+S=300
+GNAME=Ring10000-100
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRing10000-100-0.01
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRing10000-100-0.1
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRing10000-100-0.5
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRing10000-100-1
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRingB10000-100-0.01
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRingB10000-100-0.1
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRingB10000-100-0.5
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRingB10000-100-1
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+
+
+E=0
+S=300
+GNAME=Ring10000-100
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRing10000-100-0.01
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRing10000-100-0.1
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRing10000-100-0.5
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRing10000-100-1
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRingB10000-100-0.01
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRingB10000-100-0.1
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRingB10000-100-0.5
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRingB10000-100-1
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+
+E=0.01
+S=150
+GNAME=Ring10000-100
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRing10000-100-0.01
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRing10000-100-0.1
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRing10000-100-0.5
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRing10000-100-1
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRingB10000-100-0.01
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRingB10000-100-0.1
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRingB10000-100-0.5
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRingB10000-100-1
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+
+
+E=0
+S=150
+GNAME=Ring10000-100
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRing10000-100-0.01
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRing10000-100-0.1
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRing10000-100-0.5
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRing10000-100-1
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRingB10000-100-0.01
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRingB10000-100-0.1
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRingB10000-100-0.5
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
+GNAME=CRingB10000-100-1
+/coronasurveys/bin/scaleUpDirectSurveyComparator -G $GNAME -s /coronasurveys/seeds.txt -f $F -S $S -r $r -e $E >$GNAME-f$f-S$S-r$r-e$E.out &
