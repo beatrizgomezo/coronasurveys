1b6e4716dfbe8c5f88240c965b8cb3d698579113
antonio_fernandez <antonio.fernandez@imdea.org>
2021-02-11 19:03:45 +0100

    ivectors

--- code/xprize/add-deaths-hospital-cost.R
+++ code/xprize/add-deaths-hospital-cost.R
@@ -65,6 +65,19 @@ process_country_region <- function(regiondf, df, iplan, costs) {
                                        "H3_Contact.tracing","H6_Facial.Coverings")]) %*% t(as.matrix(dfcost[1, 3:14]))
   }
   
+  df[, c("C1_School.closing","C2_Workplace.closing",
+         "C3_Cancel.public.events","C4_Restrictions.on.gatherings",
+         "C5_Close.public.transport","C6_Stay.at.home.requirements",
+         "C7_Restrictions.on.internal.movement","C8_International.travel.controls",
+         "H1_Public.information.campaigns","H2_Testing.policy",
+         "H3_Contact.tracing","H6_Facial.Coverings")] <- 
+    iplan[, c("C1_School.closing","C2_Workplace.closing",
+                                        "C3_Cancel.public.events","C4_Restrictions.on.gatherings",
+                                        "C5_Close.public.transport","C6_Stay.at.home.requirements",
+                                        "C7_Restrictions.on.internal.movement","C8_International.travel.controls",
+                                        "H1_Public.information.campaigns","H2_Testing.policy",
+                                        "H3_Contact.tracing","H6_Facial.Coverings")]
+  
   return(df)
 }
 
